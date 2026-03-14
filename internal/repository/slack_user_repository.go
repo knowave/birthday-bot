21bf1b83d78b65134273b3c5c59612d487a27323
knowave <kwaktaemin98@gmail.com>
2026-03-14 21:15:54 +0900

    FEAT: batch bulk insert

--- internal/repository/slack_user_repository.go
+++ internal/repository/slack_user_repository.go
@@ -55,12 +55,24 @@ func (r *SlackUserRepository) FindAll() ([]domain.SlackUser, error) {
     if err != nil {
         return nil, err
     }
-	
+
     return users, nil
 }
 
 func (r *SlackUserRepository) Save(user *domain.SlackUser) error {
     return r.db.Save(user).Error
+}
+
+func (r *SlackUserRepository) SaveAllInBatches(slackUsers []*domain.SlackUser, batchSize int) error {
+	if len(slackUsers) == 0 {
+		return nil
+	}
+
+	if batchSize <= 0 {
+		batchSize = 100
+	}
+
+	return r.db.CreateInBatches(slackUsers, batchSize).Error
 }
 
 func (r *SlackUserRepository) Delete(id uint) error {
